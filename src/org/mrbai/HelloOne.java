262a7b8e92f73553705794b53482c3ff23126ddc
baiwencheng <baiwencheng6688@163.com>
2020-07-08 21:13:08 +0800

    首版提交

---
+++ src/org/mrbai/HelloOne.java
@@ -0,0 +1,9 @@
+package org.mrbai;
+
+public class HelloOne {
+    public static void main(String[] arg){
+        System.out.println("hello idea");
+        System.out.println("hello idea1");
+        System.out.println("hello idea2");
+    }
+}
